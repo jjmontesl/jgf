94dfad649fd3c4c4bdb848f626ad24c713d43cd4
jjmontes <jjmontes@79b687b0-a941-11dd-a8c6-677c70d10fe4>
2008-12-05 20:44:04 +0000

    Actions are now LogicStates (LogicAction). No ActionManager is needed. ControllerView is now part of the ControllerAction.

---
+++ trunk/jgf/src/example/java/net/jgf/example/tanks/logic/NewGameAction.java
@@ -0,0 +1,68 @@
+
+package net.jgf.example.tanks.logic;
+
+import net.jgf.config.Configurable;
+import net.jgf.core.state.StateUtil;
+import net.jgf.entity.EntityGroup;
+import net.jgf.jme.scene.DefaultJmeScene;
+import net.jgf.loader.FileChainLoader;
+import net.jgf.logic.LogicState;
+import net.jgf.logic.action.BaseLogicAction;
+import net.jgf.scene.Scene;
+import net.jgf.scene.SceneManager;
+import net.jgf.system.Jgf;
+import net.jgf.view.ViewState;
+
+import org.apache.log4j.Logger;
+
+
+/**
+ *
+ */
+@Configurable
+public class NewGameAction extends BaseLogicAction {
+
+	/**
+	 * Class logger
+	 */
+	private static final Logger logger = Logger.getLogger(NewGameAction.class);
+
+	/* (non-Javadoc)
+	 * @see net.jgf.logic.BaseLogicState#activate()
+	 */
+	@Override
+	public void perform(String action) {
+
+		logger.info ("Starting new tanks game (logic)");
+
+		// Prepare scene
+		SceneManager sceneManager = Jgf.getDirectory().getObjectAs("scene/manager", SceneManager.class);
+		FileChainLoader<Scene> sceneLoader = Jgf.getDirectory().getObjectAs("loader/scene", FileChainLoader.class);
+		DefaultJmeScene scene =(DefaultJmeScene) sceneLoader.load(
+				"FileChainLoader.resourceUrl=tanks/level/mission1.xml"
+		);
+		sceneManager.setScene(scene);
+		Jgf.getDirectory().addObject(scene.getId(), scene);
+
+		// Set a camera
+		//SceneRenderView sceneRenderView = Jgf.getDirectory().getObjectAs("view/root/level/fight/scene", SceneRenderView.class);
+		//sceneRenderView.setCamera(scene.getCameraControllers().getCameraController("scene/camera/test"));
+
+		// Spawn player
+		SpawnLogic spawnLogic = Jgf.getDirectory().getObjectAs("logic/root/ingame/spawn", SpawnLogic.class);
+		StateUtil.loadAndActivate(spawnLogic);
+		spawnLogic.spawnPlayer();
+
+		// Prepare entities
+		EntityGroup rootEntity = Jgf.getDirectory().getObjectAs("entity/root", EntityGroup.class);
+		StateUtil.loadAndActivate(rootEntity);
+
+		// Activate next view and logic
+		LogicState inGameLogic = Jgf.getDirectory().getObjectAs("logic/root/ingame", LogicState.class);
+		StateUtil.loadAndActivate(inGameLogic);
+		ViewState levelView = Jgf.getDirectory().getObjectAs("view/root/level", ViewState.class);
+		StateUtil.loadAndActivate(levelView);
+
+	}
+
+}
