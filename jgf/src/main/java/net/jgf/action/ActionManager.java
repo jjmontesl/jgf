5811c8355b8e8732286279042a4607603e5198ba
jjmontes <jjmontes@gmail.com>
2008-12-05 20:33:33 +0000

    Actions are now LogicStates (LogicAction). No ActionManager is needed. ControllerView is now part of the ControllerAction.

--- jgf/src/main/java/net/jgf/action/ActionManager.java
+++
@@ -1,9 +0,0 @@
-package net.jgf.action;
-
-public interface ActionManager {
-
-	/**
-	 */
-	public Action getAction(String name);
-
-}
--- jgf/src/main/java/net/jgf/action/BaseAction.java
+++
@@ -1,9 +0,0 @@
-package net.jgf.action;
-
-import net.jgf.core.component.BaseComponent;
-
-public abstract class BaseAction extends BaseComponent implements Action {
-
-	public abstract void perform();
-
-}
